8f2151e39d73a47eecbcd0218254c2bcb5da8a9c
Johann Richard <johann.richard@mycvs.org>
2017-11-11 20:10:55 +0100

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 docker rm -f websafety-config websafety
 docker rmi -f johannrichard/websafety:armhf-testing 
-docker build --force-rm --no-cache --rm=true -t johannrichard/websafety:armhf-testing .
+docker build -t johannrichard/websafety:armhf-testing .
