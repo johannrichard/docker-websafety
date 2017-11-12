5930eb6bc77f964b808b19f0995a8b88634153b3
Johann Richard <johann.richard@mycvs.org>
2017-11-12 18:03:01 +0100

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,3 @@
 docker rm -f websafety-config websafety
-docker rmi -f diladele/websafety:5.0
-docker build --force-rm --no-cache --rm=true -t diladele/websafety:5.0 .
+docker rmi -f johannrichard/websafety:armhf-baseimage-testing 
+docker build -t johannrichard/websafety:armhf-baseimage-testing .
